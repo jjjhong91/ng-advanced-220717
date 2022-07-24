9b45bf8b801f3c8f73af015456cc367cf0b5259a
Joe Jhong <thank111222333@gmail.com>
2022-07-24 16:34:02 +0800

    練習直接取用 this.form 來取得 FormGroup 物件的 valid 狀態

--- src/app/login2/login2.component.ts
+++ src/app/login2/login2.component.ts
@@ -1,54 +1,85 @@
-import { Page1Component } from './../page1/page1.component';
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { FormArray, FormBuilder, FormControl, FormGroup, NgForm, NgModel, UntypedFormGroup, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormArray,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  FormGroupDirective,
+  UntypedFormGroup,
+  Validators,
+} from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   templateUrl: './login2.component.html',
-  styleUrls: ['./login2.component.css']
+  styleUrls: ['./login2.component.css'],
 })
-export class Login2Component implements OnInit, OnDestroy {
-
+export class Login2Component implements OnInit {
   data = {
-    email: 'test@example.com',
-    password: '1234',
-    // city: 'Taipei',
-    isRememberMe: true
-  }
+    email: 'user@example.com',
+    password: '123123',
+    isRememberMe: true,
+    profiles: [
+      {
+        city: 'Taipei',
+        tel: '0988-888888',
+      },
+      {
+        city: '台中',
+        tel: '0944-444444',
+      },
+      {
+        city: 'Kaoshuang',
+        tel: '0911111111',
+      },
+    ],
+  };
 
   orig_body_className = document.body.className;
 
   form = this.fb.group({
-    email: this.fb.control('', {
+    email: this.fb.nonNullable.control('', {
       validators: [Validators.required, Validators.email],
       updateOn: 'blur',
     }),
     password: this.fb.control('', {
-      validators: [Validators.required, Validators.minLength(6), Validators.maxLength(32)]
+      validators: [
+        Validators.required,
+        Validators.minLength(6),
+        Validators.maxLength(32),
+      ],
     }),
-    isRememberMe: this.fb.control(true, {
-    }),
-
+    isRememberMe: this.fb.control(true, {}),
     profiles: this.fb.array([
-        this.makeProfile('Taipei','0900-000000'),
-        this.makeProfile('Taichung','0900-111111')
-    ])
+      this.makeProfile('Taipei', '0988-888888'),
+      this.makeProfile('台中', '0944-444444'),
+    ]),
   });
 
   makeProfile(city: string, tel: string) {
     return this.fb.group({
-      city: this.fb.control(city, { validators: [Validators.required]}),
-      tel: this.fb.control(tel, { validators: [Validators.required]}),
-    })
+      city: this.fb.control(city, { validators: [Validators.required] }),
+      tel: this.fb.control(tel, { validators: [Validators.required] }),
+    });
   }
 
-  constructor(private router: Router, private route: ActivatedRoute, private fb: FormBuilder) { }
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private fb: FormBuilder
+  ) {}
+
   ngOnInit(): void {
     document.body.className = 'bg-gradient-primary';
 
     setTimeout(() => {
-      // this.form.setValue(this.data);
-      this.form.patchValue(this.data);
+
+      this.form.controls.profiles.clear();
+      this.data.profiles.forEach(profile => {
+        this.form.controls.profiles.push(this.makeProfile(profile.city, profile.tel));
+      });
+      this.form.setValue(this.data);
+      // this.form.patchValue(this.data);
     }, 2000);
   }
 
@@ -73,35 +104,33 @@ export class Login2Component implements OnInit, OnDestroy {
   }
 
   addProfile() {
-    this.form.controls.profiles.push(this.makeProfile('',''));
+    this.form.controls.profiles.push(this.makeProfile('', ''));
   }
 
-  // login(form: NgForm) {
+  doLogin() {
+    if (this.form.valid) {
+      localStorage.setItem('apikey', 'TEST');
+      var url = this.route.snapshot.queryParamMap.get('returnUrl') || '/';
+      this.router.navigateByUrl(url);
+      this.router.navigate(['/'], {
+        state: {},
+      });
+    }
+  }
 
-  //   if (form.valid) {
-  //     localStorage.setItem('token', '123');
-  //     var url = this.route.snapshot.queryParamMap.get('returnUrl') || '/';
-  //     this.router.navigateByUrl(url);
-  //     this.router.navigate(['/'], {
-  //       state: []
-  //     });
-  //   }
+  // isInvalid(control: NgModel, form: NgForm) {
+  //   return control.invalid && (control.touched || form.submitted);
   // }
 
-  isInVaild(control: NgModel, form: NgForm) {
-    return control.invalid && (control.touched || form.submitted);
-  }
-
-  isVaild(control: NgModel) {
-    return control.valid;
-  }
+  // isValid(control: NgModel) {
+  //   return control.valid;
+  // }
 
   // disableField(control: NgModel) {
   //   if (control.disabled) {
   //     control.control.enable();
-  //   }else {
+  //   } else {
   //     control.control.disable();
   //   }
   // }
-
 }
